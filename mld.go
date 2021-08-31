945573fc71c6319a2de440cc6c62d2375298b6c9
mox692 <55653825+mox692@users.noreply.github.com>
2021-08-31 23:35:14 +0900

    wip

--- mld.go
+++ mld.go
@@ -8,10 +8,45 @@ import (
 	"os"
 )
 
+type LoadSeg string
+
 const (
 	ExitSuccess = iota
 	ExitFail
 )
+
+func setSection()
+
+// sectionを走査して、
+// 	1 data, bssは消す
+// 	2 sym, str系のsectionを書き換える
+// を行う.
+func ReWriteSection(sections []*elf.Section, secfile *os.File) error {
+	for _, sec := range sections {
+		fmt.Printf("secname: %s  ", sec.Name)
+		d, err := sec.Data()
+
+		switch sec.Name {
+		case ".data":
+		case ".bss":
+		case ".symtab":
+			d = rewriteSection(".symtab", d)
+			_, err = secfile.Write(d)
+		case ".strtab":
+			d = rewriteSection(".symtab", d)
+			_, err = secfile.Write(d)
+		case ".shstrtab":
+			d = rewriteSection(".symtab", d)
+			_, err = secfile.Write(d)
+		default:
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println()
+	}
+	return nil
+}
 
 func Run() int {
 
@@ -32,46 +67,34 @@ func Run() int {
 	// defer os.Remove(tmpFile.Name())
 
 	// 各sectionに対してloop, tmp_secに書き込む
-	for _, sec := range f.Sections {
-		fmt.Printf("secname: %s  ", sec.Name)
-		d, err := sec.Data()
+	err = ReWriteSection(f.Sections, secFile)
 
-		switch sec.Name {
-		case ".data":
-		case ".bss":
-		case ".symtab":
-			d = rewriteSection(".symtab", d)
-			_, err = secFile.Write(d)
-		case ".strtab":
-			d = rewriteSection(".symtab", d)
-			_, err = secFile.Write(d)
-		case ".shstrtab":
-			d = rewriteSection(".symtab", d)
-			_, err = secFile.Write(d)
-		default:
-		}
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERR: %s", err.Error())
-			return ExitFail
-		}
-		fmt.Println()
+	_, err = createProgramHeader(f.Sections)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERR: %s", err.Error())
+		return ExitFail
 	}
-
-	// program headerの数を予め割り出しておく必要がある
-	progNum := getProgHdrNum(f.Sections)
 
 	proFile, err := os.Create("tmp_pro")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERR: %s", err.Error())
 		return ExitFail
 	}
-	err = createProgramHeader(f.Sections, proFile, progNum)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERR: %s", err.Error())
-		return ExitFail
-	}
+
+	writeAll(secFile, proFile)
 
 	return ExitSuccess
+}
+
+// wip
+func writeAll(secfile, profile *os.File) {
+	// write elf header
+
+	// write program header
+
+	// write each section
+
+	// write section header table
 }
 
 var progTable = map[string]struct{}{".text": {}}
@@ -99,33 +122,118 @@ func rewriteSection(section string, data []byte) []byte {
 	return data
 }
 
+type SegmentType string
+
+type SegEnt struct {
+	segType      SegmentType    // segment type
+	section      []*elf.Section // そのsegmentに属してるsection
+	segEntMemsz  uint64
+	segEntFilesz uint64
+}
+
+const (
+	// .text .note.gnu.build-id
+	TEXT SegmentType = ".text"
+	// .note.gnu.build-id
+	NOTE_GNU_BUILD_ID SegmentType = ".note.gnu.build-id"
+)
+
+// sectionの配列を受け取って、それに応じたSegEntの配列を返す
+func getSegEnt(sections []*elf.Section) ([]*SegEnt, error) {
+	// すでにSegEntを作成したsegmentはこのtableに入れておく.
+	segTypeTable := make(map[SegmentType]*SegEnt)
+
+	for _, v := range sections {
+		switch v.Name {
+		case ".text":
+			if _, ok := segTypeTable[TEXT]; !ok {
+				segTypeTable[TEXT] = &SegEnt{
+					segType:      TEXT,
+					section:      []*elf.Section{v},
+					segEntMemsz:  v.Size,
+					segEntFilesz: v.Size,
+				}
+				continue
+			}
+			segTypeTable[TEXT].section = append(segTypeTable[TEXT].section, v)
+			segTypeTable[TEXT].segEntMemsz += v.Size
+			segTypeTable[TEXT].segEntFilesz += v.Size
+		case ".note.gnu.build-id":
+			// NOTE_GNU_BUILD_IDに関して
+			if _, ok := segTypeTable[NOTE_GNU_BUILD_ID]; !ok {
+				segTypeTable[NOTE_GNU_BUILD_ID] = &SegEnt{
+					segType:      NOTE_GNU_BUILD_ID,
+					section:      []*elf.Section{v},
+					segEntMemsz:  v.Size,
+					segEntFilesz: v.Size,
+				}
+			} else {
+				segTypeTable[NOTE_GNU_BUILD_ID].section = append(segTypeTable[NOTE_GNU_BUILD_ID].section, v)
+				segTypeTable[NOTE_GNU_BUILD_ID].segEntMemsz += v.Size
+				segTypeTable[NOTE_GNU_BUILD_ID].segEntFilesz += v.Size
+			}
+			// TEXTに関して
+			if _, ok := segTypeTable[TEXT]; !ok {
+				segTypeTable[TEXT] = &SegEnt{
+					segType:      TEXT,
+					section:      []*elf.Section{v},
+					segEntMemsz:  v.Size,
+					segEntFilesz: v.Size,
+				}
+				continue
+			}
+			segTypeTable[TEXT].section = append(segTypeTable[TEXT].section, v)
+			segTypeTable[TEXT].segEntMemsz += v.Size
+			segTypeTable[TEXT].segEntFilesz += v.Size
+		}
+	}
+
+	segEnt := []*SegEnt{}
+	for _, v := range segTypeTable {
+		segEnt = append(segEnt, v)
+	}
+
+	return segEnt, nil
+}
+
 // sectionを走査して、load可能sectionに対しては
-// それに対応するprogram headerを作成する
-func createProgramHeader(sections []*elf.Section, proFile *os.File, progNum int) error {
+// それに対応するprogram headerを作成して返す
+func createProgramHeader(sections []*elf.Section) ([]*ProgHeader, error) {
 	// TODO: hardcode
 	var addrOff uint64 = 0x400000
+
+	// sectionを一通り走査して、segEntの配列を得る
+	_, err := getSegEnt(sections)
+	if err != nil {
+		return nil, err
+	}
 
 	for _, v := range sections {
 		switch v.Name {
 		case ".text":
 			// textはload先segmentの中でも、先頭に配置する
 			p := ProgHeader{
-				Type:   PT_LOAD,
-				Flags:  PF_R | PF_X,
-				Off:    0x0,
-				Vaddr:  addrOff,
-				Paddr:  addrOff,
-				Filesz: uint64(progNum) * 56, // TODO: hard
-				Memsz:  uint64(progNum) * 56, // TODO: hard
-				Align:  0x200000,             // TODO: hard
+				Type:  PT_LOAD,
+				Flags: PF_R | PF_X,
+				Off:   0x0,
+				Vaddr: addrOff,
+				Paddr: addrOff,
+				// Filesz: uint64(progNum) * 56, // TODO: hard
+				// // Memsz:  uint64(progNum) * 56, // TODO: hard
+				Align: 0x200000, // TODO: hard
 			}
 			buf := new(bytes.Buffer)
+			// bufにbinaryを書く
 			err := binary.Write(buf, binary.LittleEndian, p)
 			if err != nil {
-				return err
+				return nil, err
 			}
-			proFile.WriteAt(buf.Bytes(), 0)
+			// bufのbinaryをそのままproFileに書く
+			// _, err = proFile.WriteAt(buf.Bytes(), 0)
+			// if err != nil {
+			// 	return nil, err
+			// }
 		}
 	}
-	return nil
+	return nil, nil
 }
